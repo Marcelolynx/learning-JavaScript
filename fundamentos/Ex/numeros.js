a69b510fb41bc963b151f1fb604891781ad91bdb
Marcelo Oliveira <marcelolynx@Mac-mini-de-Marcelo.local>
2018-05-29 20:28:39 -0400

    5555

--- fundamentos/Ex/numeros.js
+++ fundamentos/Ex/numeros.js
@@ -16,4 +16,6 @@ const media = total / (peso1 + peso2)
 console.log(media.toFixed(2))
 console.log(media.toString(2)) // transforma em binário
 console.log(typeof media)
+console.log(typeof Number) 
+ 
 console.log(typeof Number)
